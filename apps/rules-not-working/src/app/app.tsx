498970c476826210230ac2f0173ba438807b11ac
Eduard Stankovič <eduard.stankovic@gmail.com>
2023-08-22 11:06:15 +0200

    Initial commit

---
+++ apps/rules-not-working/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="rules-not-working" />
+    </div>
+  );
+}
+
+export default App;
